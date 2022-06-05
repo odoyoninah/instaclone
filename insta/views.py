836e27a2fe16dabf82eceb90630dcb4293f6690c
odoyonina <odoyoninah@gmail.com>
2022-06-06 00:54:40 +0300

    create render for login and sign up

--- insta/views.py
+++ insta/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render
+import email
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
 def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        email = request.POST['email']
+        myuser = User.objects.create_user(username,email,pass1)
+        myuser.save()
+        messages.success(request,'You have successfully signed up!')
+
+        return redirect('login')
+
+
+
+
+
     return render(request,'signup.html')
 
 def login(request):
