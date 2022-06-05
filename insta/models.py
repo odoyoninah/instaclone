dd1a74056775d6422ec17c9eb83622a75e02e3ae
odoyonina <odoyoninah@gmail.com>
2022-06-06 02:13:43 +0300

    create class comment

--- insta/models.py
+++ insta/models.py
@@ -4,3 +4,31 @@ from django.db.models.signals import post_save,post_delete
 
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to='profile_pics',blank=True)
+    bio = models.TextField(max_length=100,blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+class Image(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images')
+    caption = models.CharField(max_length=100)
+    likes = models.IntegerField(default=0)
+    comments = models.IntegerField(default=0)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.caption
+
+class Comment(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    image = models.ForeignKey(Image,on_delete=models.CASCADE)
+    comment = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.comment
+    
