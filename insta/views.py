173b1e773c8e20f45f9bafed701d17ed8425755b
odoyonina <odoyoninah@gmail.com>
2022-06-06 00:02:00 +0300

    create views for login and signup

--- insta/views.py
+++ insta/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def signup(request):
+    return render(request,'signup.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def logout(request):
+    return render(request,'logout.html')
