c046b6df5f30794664c804aa11d02a42900d9029
odoyonina <odoyoninah@gmail.com>
2022-06-06 11:11:38 +0300

    add profile pics

--- insta/admin.py
+++ insta/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User,Image,Comment,Like,Follow
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Image)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Follow)
