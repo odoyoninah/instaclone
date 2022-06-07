d226ccaffdf8530c7d1d3a2d6a36795b54473d1d
odoyonina <odoyoninah@gmail.com>
2022-06-07 23:15:38 +0300

    create static folder

--- instagram/settings.py
+++ instagram/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # configuring the location for media
 MEDIA_URL = '/media/'
